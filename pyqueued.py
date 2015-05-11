62a21da83153294586a9fd20eb6a5e880ebed406
Martin Czygan <martin.czygan@gmail.com>
2015-05-11 07:19:43 +0200

    general cleanup

--- pyqueued.py
+++ pyqueued.py
@@ -1,40 +1,37 @@
 # coding: utf-8
 
+import json
 import os
 import requests
-import json
-
-try:
-    from urllib.parse import urlparse
-except ImportError:
-    from urlparse import urlparse
-
-def parse_location(location):
-    """ Return hostport, queue, id from url. """
-    result = urlparse(location)
-    queue, id = result.path.strip('/').split('/')
-    return result.netloc, queue, id
 
 class Client:
-    """ A client object. """
+    """
+    A Queued client object.
+    """
 
     def __init__(self, host='localhost', port=5353):
         self.host = host
         self.port = port
 
     def queue_url(self, queue):
-        """ Get the URL to a queue. """
+        """
+        Get the URL to a queue.
+        """
         return "http://{0}:{1}/{2}".format(self.host, self.port, queue)
 
     def enqueue(self, queue, msg):
-        """ Enqueue a message on a given queue. """
+        """
+        Enqueue a message on a given queue.
+        """
         r = requests.post(self.queue_url(queue), data=msg)
         if not r.status_code == 201:
             raise RuntimeError("enqueue failed: %s" % r)
         return r.headers['Location']
 
     def dequeue(self, queue, wait=None, timeout=None):
-        """ Dequeue a message on a given queue. """
+        """
+        Dequeue a message on a given queue.
+        """
         params = dict([(k, v) for k, v in (('wait', wait), ('timeout', timeout)) if v is not None])
         r = requests.post(os.path.join(self.queue_url(queue), 'dequeue'), params=params)
         if not r.status_code == 200:
@@ -42,18 +39,35 @@ class Client:
         return r.text, r.headers['Location']
 
     def complete(self, queue, id):
-        """ Remove an item from a queue. """
+        """
+        Remove an item from a queue, given its id.
+        """
+        r = requests.delete(os.path.join(self.queue_url(queue), str(id)))
+        if not r.status_code == 204:
+            raise RuntimeError("complete failed: %s" % r)
+
+    def complete_by_url(self, url):
+        """
+        Remove an item from a queue, given the items full url.
+        """
+        queue, id = url.split('/')[-2:]
         r = requests.delete(os.path.join(self.queue_url(queue), id))
         if not r.status_code == 204:
             raise RuntimeError("complete failed: %s" % r)
 
     def get(self, queue, id):
+        """
+        Get an item by queue and id.
+        """
         r = requests.get(os.path.join(self.queue_url(queue), id))
         if not r.status_code == 200:
             raise RuntimeError("get failed: %s" % r)
         return r.text
 
     def stats(self, queue):
+        """
+        Return stats for a queue.
+        """
         r = requests.get(self.queue_url(queue))
         if not r.status_code == 200:
             raise RuntimeError("stats failed: %s" % r)
--- test_pyqueued.py
+++ test_pyqueued.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from pyqueued import Client, parse_location
+import pyqueued
 import random
 import requests
 import time
@@ -9,22 +9,15 @@ import unittest
 def random_queue_name():
     return "test_pyqueued_%d" % random.randint(0, 99999999)
 
-class TestUtils(unittest.TestCase):
-    def test_parse_location(self):
-        hostport, q, id = parse_location("http://localhost:5353/queue/id")
-        self.assertEquals(hostport, "localhost:5353")
-        self.assertEquals(q, "queue")
-        self.assertEquals(id, "id")
-
 class TestClient(unittest.TestCase):
 
     def test_init(self):
-        c = Client()
+        c = pyqueued.Client()
         self.assertTrue(c is not None)
 
     def test_enqueue(self):
         Q = random_queue_name()
-        c = Client()
+        c = pyqueued.Client()
         location = c.enqueue(Q, "Hello")
         self.assertTrue(location.startswith(c.queue_url(Q)))
         _, _ = c.dequeue(Q)
@@ -32,7 +25,7 @@ class TestClient(unittest.TestCase):
 
     def test_dequeue(self):
         Q = random_queue_name()
-        c = Client()
+        c = pyqueued.Client()
         c.enqueue(Q, "Hello")
         msg, location = c.dequeue(Q)
         self.assertEquals(msg, "Hello")
@@ -40,7 +33,7 @@ class TestClient(unittest.TestCase):
 
     def test_dequeue_timeout(self):
         Q = random_queue_name()
-        c = Client()
+        c = pyqueued.Client()
         c.enqueue(Q, "Hello")
         msg, location = c.dequeue(Q, timeout=1)
         self.assertEquals(msg, "Hello")
@@ -52,7 +45,7 @@ class TestClient(unittest.TestCase):
 
     def test_dequeue_timeout_expire(self):
         Q = random_queue_name()
-        c = Client()
+        c = pyqueued.Client()
         c.enqueue(Q, "Hello")
         msg, location = c.dequeue(Q, timeout=1)
         self.assertEquals(msg, "Hello")
@@ -64,13 +57,13 @@ class TestClient(unittest.TestCase):
 
     def test_dequeue_wait(self):
         Q = random_queue_name()
-        c = Client()
+        c = pyqueued.Client()
         with self.assertRaises(RuntimeError):
             msg, location = c.dequeue(Q, wait=0.2)
 
     def test_get_requests(self):
         Q = random_queue_name()
-        c = Client()
+        c = pyqueued.Client()
         location = c.enqueue(Q, "Hello")
         r = requests.get(location)
         self.assertEquals(r.status_code, 200)
@@ -78,7 +71,23 @@ class TestClient(unittest.TestCase):
 
     def test_get(self):
         Q = random_queue_name()
-        c = Client()
+        c = pyqueued.Client()
         location = c.enqueue(Q, "Hello")
         id = location.split('/')[-1]
         self.assertEquals(c.get(Q, id), "Hello")
+
+    def test_complete_by_url(self):
+        Q = random_queue_name()
+        c = pyqueued.Client()
+        location = c.enqueue(Q, "Hello")
+        msg, location = c.dequeue(Q)
+        with self.assertRaises(RuntimeError):
+            c.complete_by_url(location)
+
+    def test_complete(self):
+        Q = random_queue_name()
+        c = pyqueued.Client()
+        location = c.enqueue(Q, "Hello")
+        msg, location = c.dequeue(Q)
+        with self.assertRaises(RuntimeError):
+            c.complete(*location.split('/')[-2:])
